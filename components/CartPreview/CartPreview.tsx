292381cb138baa2ed2b0448138e18bcd919c48af
Borzoo Moazami <borzoomoazami94@gmail.com>
2023-07-06 14:14:35 +0330

    fix: remove TODO comment that was done before

--- components/CartPreview/CartPreview.tsx
+++ components/CartPreview/CartPreview.tsx
@@ -21,7 +21,6 @@ import { DeleteOutline } from '@mui/icons-material'
 import style from './CartPreview.module.scss'
 import classNames from 'classnames'
 
-// TODO: Consider better placement for tablet screen
 export default function CartPreview({ closeCart, open }: CartPreviewProps) {
   const { totalItemCount, cartItems, removeCartItem } = useCart()
   const tablet = useMediaQuery((theme: any) => theme.breakpoints.up('sm'))
