4736c18f8805462e8e2a1f3ca690a7daa9c03e20
BorzooMV <69573788+BorzooMV@users.noreply.github.com>
2023-07-21 03:10:43 +0000

    Deploying to gh-pages from @ BorzooMV/ecommerce-product-page@6d7d026557e584ac320af7e314d3838628a263b0 🚀

---
+++ _next/static/7qL_HoKW3oKdczC6pSiih/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/9deb1c030c02fcf1.css","static/chunks/pages/index-cd1ee3bee9107e04.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
